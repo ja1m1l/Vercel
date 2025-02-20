c7c9c936b93542dea2ef93437c4100646f285da2
ja1m1l <Jaimil.achpr22@sinhgad.edu>
2025-02-20 19:55:31 +0530

    Delete healthcare-system directory

--- healthcare-system/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center p-4 bg-background">
-      <p>&copy; 2023 Healthcare Management System. All rights reserved.</p>
-    </footer>
-  )
-}
-
